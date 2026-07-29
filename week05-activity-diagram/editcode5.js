b9d5660ae8bd38eca7280c046b352ee5ca883af5
nazhat <nazhathafidzaljauzi2007@gmail.com>
2026-07-30 03:46:24 +0700

    menambahkan guard 2 persetujuan khusus buku referensi

--- week05-activity-diagram/editcode5.js
+++ week05-activity-diagram/editcode5.js
@@ -24,3 +24,19 @@ constructor(id, nama, tunggakan = 0, peminjamanAktif = 0) {
     this.peminjamanAktif = peminjamanAktif;
 }
 
+  // Method enkapsulasi untuk menambah jumlah peminjaman aktif
+tambahPeminjamanAktif() {
+    this.peminjamanAktif += 1;
+}
+}
+
+function prosesPeminjaman(buku, anggota, disetujuiKhusus = false) {
+console.log(`[MULAI] ${anggota.nama} mengajukan "${buku.judul}" (${buku.jenis})`);
+
+  // Guard 1: Stok Buku
+console.log("[CEK] Buku tersedia?");
+if (buku.eksemplarTersedia <= 0) {
+    console.log("[TOLAK] Stok buku habis.\n");
+    return { berhasil: false, alasan: "STOK_HABIS" };
+}
+
